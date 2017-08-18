e8c1374b524e7e218b213465a98bab48a7e128fa
Jens Langerak <J.Langerak@student.tudelft.nl>
2017-08-18 16:31:09 +0200

    Create texture and apply to plane.

--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextureGenerator : MonoBehaviour {
+public class TextureGenerator {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static public void SetTexture(Material material)
+    {
+        var texture = new Texture2D(256, 256);
+        material.mainTexture = texture;
+        var topLeft = Color.red;
+        var topRight = Color.green;
+        var bottomLeft = Color.yellow;
+        var bottomRight = Color.blue;
+
+
+        for (int y = 0; y < texture.height; y++)
+        {
+            float top = y / (256.0f);
+            Color left = Color.Lerp(bottomLeft, topLeft, top);
+            Color right = Color.Lerp(bottomRight, topRight, top);
+
+            for (int x = 0; x < texture.width; x++)
+            {
+                float grad = x / (256.0f);
+
+                Color color = Color.Lerp(left, right, grad);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+    }
+
 }
