ead1faa00409e03fe42452aa15d656d452083fb7
Jens Langerak <J.Langerak@student.tudelft.nl>
2017-08-18 15:55:42 +0200

    Create directory structure.

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starting launcher!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
